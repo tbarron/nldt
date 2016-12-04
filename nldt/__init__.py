bc4b93996097d1a3b22c2fe85ed05e1ee8850933
Tom Barron <tom.barron@comcast.net>
2016-12-04 11:05:30 -0500

    - Applying Tomdoc principles (tomdoc.org) to function documentation. That    is, public and private functions, methods, and data should be    distinguished. Python does this by naming private items with a leading    underscore. Each function prolog should include a description of what    the function does and an example of its use. In some cases, this is not    yet complete, but the deficiencies are marked

--- nldt/__init__.py
+++ nldt/__init__.py
@@ -1,19 +1,28 @@
+"""
+Natural Language Date and Time package
+
+This module provides a simple interfaace to Python's time and date processing
+machinery.
+"""
 import numbers
 import pdb
 import re
 import time
 
 
-DAY = 24*3600
-WEEK = 7 * DAY
+DAY = 24*3600      # !@! should be private
+WEEK = 7 * DAY     # !@! should be private
+
 
 # -----------------------------------------------------------------------------
 def dst():
     """
-    Public
+    Return True or False - daylight savings time is in force or not
 
-    Return True or False, indicating whether or not daylight savings time is in
-    force
+    Examples:
+        >>> import nldt
+        >>> nldt.dst()
+        False
     """
     now = time.localtime()
     return now.tm_isdst == 1
@@ -22,9 +31,16 @@ def dst():
 # -----------------------------------------------------------------------------
 def timezone():
     """
-    Public
-
     Return the locally configured timezone name
+
+    The timezone may vary with whether DST is in effect or not. The example
+    show a timezone of 'EST' for Eastern Standard Time. At times of the year
+    when DST is in effect, the timezone is EDT.
+
+    Examples:
+        >>> import nldt
+        >>> nldt.timezone
+        EST
     """
     now = time.localtime()
     rval = time.tzname[now.tm_isdst]
@@ -32,22 +48,40 @@ def timezone():
 
 
 # -----------------------------------------------------------------------------
-WEEKDAYS = {'mon': 0, 0: 'mon',
-            'tue': 1, 1: 'tue',
-            'wed': 2, 2: 'wed',
-            'thu': 3, 3: 'thu',
-            'fri': 4, 4: 'fri',
-            'sat': 5, 5: 'sat',
-            'sun': 6, 6: 'sun'}
+_WEEKDAYS = {'mon': 0, 0: 'mon',
+             'tue': 1, 1: 'tue',
+             'wed': 2, 2: 'wed',
+             'thu': 3, 3: 'thu',
+             'fri': 4, 4: 'fri',
+             'sat': 5, 5: 'sat',
+             'sun': 6, 6: 'sun'}
 
 
 # -----------------------------------------------------------------------------
 def weekday_index(weekday):
     """
-    Return the numeric index of *weekday*
+    Given weekday name, return index (Monday == 0; Sunday == 6)
+
+    *weekday* - int between 0 and 6 => three letter day name string
+                three letter day name string => int between 0 (mon) and 6 (sun)
+
+    Example:
+        >>> import nldt
+
+        # Compute weekday delta
+        >>> diff = nldt.weekday_index('thu') - nldt.weekday_index('mon')
+
+        # diff == number of days to jump from monday to thursday
+        >>> diff = nldt.weekday_index('mon') - nldt.weekday_index('thu')
+
+        # diff == number of days to jump from thursday to monday (negative)
+        # General:
+        >>> diff = nldt.weekday_index(end_day) - nldt.weekday_index(start_day)
+        # diff == days to jump from start to end, either forward or backward
+        # 7 + diff == days to jump from start to end in succeeding week
     """
     idx = weekday.lower()[0:3]
-    rval = WEEKDAYS[idx]
+    rval = _WEEKDAYS[idx]
     return rval
 
 
@@ -123,10 +157,37 @@ class moment(object):
     # -------------------------------------------------------------------------
     def __init__(self, *args):
         """
-        Expected, optional arguments are:
-         - a time offset or specification (e.g., 'yesterday', 'tomorrow', 'next
-           week', 'Dec 3 1972', an epoch time)
-         - a parsing format
+        Construct a moment object
+
+        *args*:
+            empty: object represents current time at instantiation
+            one element: may be a date/time spec matching one of the formats in
+                self.formats or a natural language expression describing the
+                time of interest (see Examples).
+            two elements: args[0] is a date/time spec, args[1] is a format
+                describing args[0]
+
+        Examples:
+            >>> import nldt
+            # current time
+            >>> now = nldt.moment()
+            >>> now()
+            '2016-12-04'
+            # listed format
+            >>> new_year_day = nldt.moment('2001-01-01')
+            >>> new_year_day()
+            '2001-01-01'
+            # natural language
+            >>> yesterday = nldt.moment('yesterday')
+            >>> yesterday()
+            '2016-12-03'
+            >>> nw = nldt.moment('next week')
+            >>> nw()
+            '2016-12-05'
+            # specified format
+            >>> then = nldt.moment('Dec 29 2016', '%b %m %Y')
+            >>> then()
+            '2016-12-29'
         """
         if 'fixedup' not in self.nldict:
             for key in self.nldict:
@@ -147,8 +208,20 @@ class moment(object):
     # -------------------------------------------------------------------------
     def __call__(self, format=None):
         """
-        Return a string containing the stored time according to *format*. If
-        *format* is None, we use ISO format
+        Return a string representing the date of the CSM
+
+        *format*: Optional string indicating the desired output format.
+
+        Examples:
+            >>> import nldt
+            >>> a = nldt.moment()
+            # No arguments, default format (ISO)
+            >>> a()
+            '2016-12-04'
+
+            # *format* specified
+            >>> a('%Y.%m%d %H:%M:%S')
+            '2016.1204 07:16:20'
         """
         format = format or "%Y-%m-%d"
         rval = time.strftime(format, time.localtime(self.moment))
@@ -158,14 +231,42 @@ class moment(object):
     # -------------------------------------------------------------------------
     def __eq__(self, other):
         """
-        Two moment objects are equal if their moment attributes are
+        Return True or False - whether two moment objects are equal
+        implicit)
+
+        *other*: a second moment object to be compared to self
+
+        Examples:
+            >>> import nldt
+
+            # instantiated seconds apart, a and b will not be equal
+            >>> a = nldt.moment()
+            >>> b = nldt.moment()
+            >>> a == b
+            False
+
+            # however, once they are both advanced to the beginning of
+            # tomorrow, so the both record the beginning of the day at
+            # midnight, they will be equal
+            >>> a.parse('tomorrow')
+            >>> b.parse('tomorrow')
+            >>> a == b
+            True
         """
         return self.moment == other.moment
 
     # -------------------------------------------------------------------------
     def __repr__(self):
         """
-        Return a string that will regenerate this object if eval'd
+        Return a string that will regenerate this object if passed to eval()
+
+        Examples:
+            >>> import nldt
+            >>> c = nldt.moment()
+            >>> assert eval(repr(c)) == c
+            True
+            >>> print repr(c)
+            nldt.moment(1481000400)
         """
         rval = "nldt.moment({:d})".format(int(self.moment))
         return rval
@@ -174,6 +275,12 @@ class moment(object):
     def __str__(self):
         """
         Return a human-readable representation of this object
+
+        Examples:
+            >>> import nldt
+            >>> c = nldt.moment()
+            >>> print(c)
+            2016-12-04 07:31:08
         """
         return time.strftime('%Y-%m-%d %H:%M:%S',
                              time.localtime(self.moment))
@@ -181,8 +288,16 @@ class moment(object):
     # -------------------------------------------------------------------------
     def dst(self):
         """
-        Return True or False to indicate whether Daylight Savings Time is in
-        force on the stored date
+        Return True or False - whether DST is in force at the CSM
+
+        Examples:
+            >>> import nldt
+            >>> q = nldt.moment('1962.0317')
+            >>> q.dst()
+            False
+            >>> q.parse('1962.0430')
+            >>> q.dst()
+            True
         """
         tm = time.localtime(self.moment)
         return tm.tm_isdst == 1
@@ -190,14 +305,27 @@ class moment(object):
     # -------------------------------------------------------------------------
     def epoch(self):
         """
-        Return the stored date as an epoch time
+        Return the CSM as an int epoch time
+
+        Examples:
+            >>> import nldt
+            >>> q = nldt.moment()
+            >>> q.epoch()
+            1480855032
         """
         return int(self.moment)
 
     # -------------------------------------------------------------------------
     def localtime(self):
         """
-        Return the tm structure for the stored moment
+        Return the tm structure for the CSM
+
+        examples:
+            >>> import nldt
+            >>> q = nldt.moment()
+            >>> q.localtime()
+            time.struct_time(tm_year=2016, tm_mon=12, tm_mday=4, tm_hour=7,
+            tm_min=37, tm_sec=12, tm_wday=6, tm_yday=339, tm_isdst=0)
         """
         return time.localtime(self.moment)
 
@@ -209,21 +337,39 @@ class moment(object):
         spec - A date/time specification
 
         Example:
-            foo = when.when()
-            foo.parse('2013.0630')
-            foo.parse('next friday at 5')
+            >>> import nldt
+            >>> foo = nldt.moment()
+            >>> foo.parse('next friday')
+            >>> foo()
+            '2016-12-09'
+            >>> foo.parse('2013.0630')
+            >>> foo.parse('next friday')
+            >>> foo()
+            '2013-07-05'
         """
         self.moment = self.parse_return(spec)
 
     # -------------------------------------------------------------------------
     def end_of_day(self, wday_name=None):
         """
-        Compute the end of the indicated day. If not today, we assume 'next'.
+        Compute the end of the indicated day
+
+        *wday_name* - The name of the target day
+
+        If wday_name is specified, we assume 'next'. However, if wday_name
+        matches the weekday of the CSM, the calculation
+        applies to the CSM.
+
+        Example:
+            >>> import nldt
+            >>> foo = nldt.moment()
+            >>> foo.end_of_day()
+            1481345999
         """
         point = self.moment or time.time()
         if wday_name:
             now = time.localtime(point)
-            wday_num = WEEKDAYS[wday_name]
+            wday_num = _WEEKDAYS[wday_name]
             diff = (7 + wday_num - now.tm_wday) % 7
         else:
             diff = 0
@@ -233,12 +379,13 @@ class moment(object):
         ref_s = time.strftime("%Y-%m-%d 23:59:59", ref)
         anchor = time.strptime(ref_s, fmt)
         rval = time.mktime(anchor)
-        return rval
+        return int(rval)
 
     # -------------------------------------------------------------------------
     def end_of_week(self):
         """
-        Compute the end of the week.
+        Compute the end of the week based on CSM
+        !@! needs example
         """
         rval = self.end_of_day('sun')
         return rval
@@ -247,6 +394,7 @@ class moment(object):
     def end_of_month(self):
         """
         Compute the end of the month.
+        !@! needs example
         """
         rval = None
         return rval
@@ -255,6 +403,7 @@ class moment(object):
     def end_of_year(self):
         """
         Compute the end of the year.
+        !@! needs example
         """
         rval = None
         return rval
@@ -263,8 +412,9 @@ class moment(object):
     def last_weekday(self, wday_name):
         """
         Compute the epoch time of the indicated future weekday
+        !@! needs example
         """
-        wday_num = WEEKDAYS[wday_name]
+        wday_num = _WEEKDAYS[wday_name]
         ref = self.moment or time.time()
         now = time.localtime(ref)
         diff = (7 + now.tm_wday - wday_num - 1) % 7 + 1
@@ -275,6 +425,7 @@ class moment(object):
     def last_year(self):
         """
         Compute the epoch time of the indicated past year
+        !@! needs example
         """
         thisyear = time.strftime("%Y")
         lastyear = int(thisyear) - 1
@@ -287,9 +438,10 @@ class moment(object):
     def next_weekday(self, wday_name):
         """
         Compute the epoch time of the indicated future weekday
+        !@! needs example
         """
         ref = self.moment or time.time()
-        wday_num = WEEKDAYS[wday_name]
+        wday_num = _WEEKDAYS[wday_name]
         now = time.localtime()
         diff = (7 + wday_num - now.tm_wday - 1) % 7 + 1
         rval = ref + diff * DAY
@@ -299,6 +451,7 @@ class moment(object):
     def next_year(self):
         """
         Compute the epoch time of the indicated future year
+        !@! needs example
         """
         thisyear = time.strftime("%Y")
         nextyear = int(thisyear) + 1
@@ -309,6 +462,9 @@ class moment(object):
 
     # -------------------------------------------------------------------------
     def guess_format(self, spec):
+        """
+        !@! needs example and description
+        """
         tm = None
         for fmt in self.formats:
             try:
@@ -399,6 +555,15 @@ class moment(object):
     # -------------------------------------------------------------------------
     def timezone(self):
         """
+        Return the timezone based on current CSM
+
+        Example:
+            >>> a = nldt.moment('2004-06-03')
+            >>> a.timezone()
+            'EDT'
+            >>> b = nldt.moment('2005-12-15')
+            >>> b.timezone()
+            'EST'
         """
         lt = time.localtime(self.moment)
         return time.tzname[lt.tm_isdst]
@@ -406,10 +571,17 @@ class moment(object):
     # -------------------------------------------------------------------------
     def _tomorrow(self, ref=None):
         """
-        internal
+        Compute and return epoch time of tomorrow based on ref, self.moment, or
+        time.time()
 
+        Example:
+            >>> import nldt
+            >>> a = nldt.moment('2000-12-31')
+            >>> b = nldt.moment(a._tomorrow())
+            >>> b()
+            '2001-01-01'
         """
-        tmr = ref or self.moment
+        tmr = ref or self.moment or time.time()
         tmr += DAY
         rval = tmr - (tmr % DAY) + time.timezone
         return rval
@@ -417,8 +589,14 @@ class moment(object):
     # -------------------------------------------------------------------------
     def tomorrow(self):
         """
-        Return a moment object containing the moment beginning the day that
-        follows the one containing the stored moment
+        Return a moment object based on CSM's tomorrow
+
+        Example:
+            >>> import nldt
+            >>> a = ndlt.moment('2001-11-30')
+            >>> b = a.tomorrow()
+            >>> b()
+            '2001-12-01'
         """
         rval = moment(self._tomorrow())
         return rval
@@ -426,8 +604,14 @@ class moment(object):
     # -------------------------------------------------------------------------
     def yesterday(self):
         """
-        Return a moment object containing the moment beginning the day that
-        precedes the one containing the stored moment
+        Return a moment object based on CSM's yesterday
+
+        Example:
+            >>> import nldt
+            >>> a = ndlt.moment('2001-11-30')
+            >>> b = a.yesterday()
+            >>> b()
+            '2001-11-29'
         """
         yest = self.moment - DAY
         rval = moment(yest - (yest % DAY) + time.timezone)
@@ -437,6 +621,7 @@ class moment(object):
     def end_of_last_week(self, ref=None):
         """
         Return the moment that ends last week
+        !@! should be private
         """
         ref = ref or self.moment or time.time()
         ref -= WEEK
