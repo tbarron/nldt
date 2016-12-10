bb70274600798fcefbdba28ae0c27fa22083bab5
Tom Barron <tom.barron@comcast.net>
2016-12-10 04:31:45 -0500

    - Isolate numwords set up in a function

--- numberize.py
+++ numberize.py
@@ -1,20 +1,6 @@
-def text2int(textnum, numwords={}):
-    if not numwords:
-        units = [
-        "zero", "one", "two", "three", "four", "five", "six", "seven", "eight",
-        "nine", "ten", "eleven", "twelve", "thirteen", "fourteen", "fifteen",
-        "sixteen", "seventeen", "eighteen", "nineteen",
-        ]
 
-        tens = ["", "", "twenty", "thirty", "forty", "fifty", "sixty", "seventy",
-                "eighty", "ninety"]
 
-        scales = ["hundred", "thousand", "million", "billion", "trillion"]
 
-        numwords["and"] = (1, 0)
-        for idx, word in enumerate(units):  numwords[word] = (1, idx)
-        for idx, word in enumerate(tens):       numwords[word] = (1, idx * 10)
-        for idx, word in enumerate(scales): numwords[word] = (10 ** (idx * 3 or 2), 0)
 
     ordinal_words = {'first':1, 'second':2, 'third':3, 'fifth':5, 'eighth':8, 'ninth':9,
                      'twelfth':12}
@@ -42,3 +28,27 @@ def text2int(textnum, numwords={}):
             current = 0
 
     return result + current
+# -----------------------------------------------------------------------------
+def set_numwords():
+    """
+    Set up the dictionary of numeric words
+    """
+    numwords = {}
+    units = [
+        "zero", "one", "two", "three", "four", "five", "six", "seven", "eight",
+        "nine", "ten", "eleven", "twelve", "thirteen", "fourteen", "fifteen",
+        "sixteen", "seventeen", "eighteen", "nineteen",
+        ]
+
+    tens = ["", "", "twenty", "thirty", "forty", "fifty", "sixty", "seventy",
+            "eighty", "ninety"]
+
+    scales = ["hundred", "thousand", "million", "billion", "trillion"]
+
+    numwords["and"] = (1, 0)
+    for idx, word in enumerate(units):  numwords[word] = (1, idx)
+    for idx, word in enumerate(tens):       numwords[word] = (1, idx * 10)
+    for idx, word in enumerate(scales): numwords[word] = (10 ** (idx * 3 or 2), 0)
+
+    return numwords
+
