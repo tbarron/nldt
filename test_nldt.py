508e74d431077139930c6e99ec85122551ffef60
Tom Barron <tusculum@gmail.com>
2017-04-12 05:43:52 -0400

    Remove backspaces, 'nldict' and 'parse' are present

--- test_nldt.py
+++ test_nldt.py
@@ -22,12 +22,23 @@ def test_pydoc():
     """
     Verify that public items show up in pydoc output while private items do not
     """
-    present = ['__init__', 'dst', 'timezone',
+    pytest.debug_func()
+    present = ['__call__', '__eq__', '__init__', '__repr__', '__str__',
+               'dst',
+               'epoch',
+               'localtime',
+               'moment',
                'month_days', 'month_index', 'month_names',
-               'weekday_index', 'weekday_names', 'moment']
-    absent = ['_DAY', '_WEEK', '_WEEKDAYS', '_MONTHS', '_MONTH_LEN']
+               'nldict', 'parse',
+               'timezone',
+               'weekday_index', 'weekday_names']
+
+    absent = ['_DAY', '_end_of_day', '_end_of_month', '_end_of_week',
+              '_guess_format', '_MONTHS', '_MONTH_LEN', '_nl_match',
+              '_parse_return', '_WEEK', '_week_ago', '_WEEKDAYS', ]
+
     docker = pydoc.TextDoc()
-    result = docker.document(nldt)
+    result = re.sub("\x08.", "", docker.document(nldt))
     for item in present:
         assert item in result
     for item in absent:
