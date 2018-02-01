4a4a145a59890e19e1473f355dbfdfdd315ba515
Tom Barron <tusculum@gmail.com>
2018-01-31 22:44:34 -0500

    Tests for helper routines

---
+++ tests/test_helpers.py
@@ -0,0 +1,10 @@
+import nldt
+
+
+def test_hhmm():
+    """
+    Verify that nldt.hhmm() correctly converts seconds into HHMM
+    """
+    assert nldt.hhmm(-18000) == '-0500'
+    assert nldt.hhmm(18000) == '0500'
+    assert nldt.hhmm(7200) == '0200'
