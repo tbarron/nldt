e9779d01f6b2999bee17684e9df7507d2b09ad6d
Tom Barron <tusculum@gmail.com>
2018-02-03 07:10:07 -0500

    Tests to improve code coverage

--- tests/test_numberize.py
+++ tests/test_numberize.py
@@ -2,12 +2,13 @@ import numberize as num
 import pytest
 
 # -----------------------------------------------------------------------------
-@pytest.mark.parametrize("inp, exp", [('   Twas brillig and the slithe toves   ',
-                                       ('Twas', 'brillig and the slithe toves')),
-                                      ('  humpty     ',
-                                       ('humpty', None)),
-                                      ('     ',
-                                       (None, None))])
+@pytest.mark.parametrize("inp, exp",
+                         [('   Twas brillig and the slithe toves   ',
+                           ('Twas', 'brillig and the slithe toves')),
+                          ('  humpty     ', ('humpty', None)),
+                          ('     ', (None, None)),
+                          (['not a string'], (['not a string'], None)),
+                          ])
 def test_tokenize(inp, exp):
     """
     test tokenizing
@@ -15,7 +16,30 @@ def test_tokenize(inp, exp):
     pytest.debug_func()
     assert num.tokenize(inp) == exp
 
-    
+
+# -----------------------------------------------------------------------------
+def test_ordinals():
+    """
+    Test ordinal recognition
+    """
+    pytest.debug_func()
+    result = num.scan("first time second branch fortieth birthday "
+                      "seventh crotchety thirty rock")
+    assert result == [1, 'time', 2, 'branch', 40, 'birthday',
+                      7, 'crotchety', 30, 'rock']
+
+
+# -----------------------------------------------------------------------------
+def test_scale():
+    """
+    Coverage for line 87-88
+    """
+    pytest.debug_func()
+    result = num.scan("tenth ninth eighteenth two thousand "
+                      "seventy-second forty-third")
+    assert result == [39115]
+
+
 # -----------------------------------------------------------------------------
 @pytest.mark.parametrize("inp, exp", [('one', [1]),
                                       ('two', [2]),
