dfb1a2c7a1766ab700e5b454dcaeae26d5de9e67
Tom Barron <tusculum@gmail.com>
2018-02-17 05:21:02 -0500

    Add cmdl.py, test_cmdl.py to git, .gitignore nldt.egg-info

---
+++ nldt/cmdl.py
@@ -0,0 +1,49 @@
+"""
+Usage:
+    nldt [-d] [-f=<fmt>] [-w=<anchor>] [-z=<timezone>] DATE_TIME_EXPR ...
+
+Options:
+    -d, --debug            run the debugger
+    -f, --format=<fmt>     strftime-style output format
+    -w, --when=<anchor>    define 'now'
+    -z, --zone=<timezone>  'local' or explicit timezone
+
+<anchor> can be
+
+Examples:
+    $ nldt now
+    2018-02-16 11:49:13
+    $ nldt today
+    2018-02-16
+    $ nldt tomorrow
+    2018-02-17
+    $ nldt -w '2018-01-01' yesterday
+    2017-12-31
+"""
+import docopt
+import nldt
+import pdb
+# import sys
+
+
+# -----------------------------------------------------------------------------
+def main():
+    """
+    Here is where we start
+    """
+    opts = docopt.docopt(__doc__)
+    if opts['--debug']:
+        pdb.set_trace()
+    expr = " ".join(opts['DATE_TIME_EXPR'])
+    fmt = opts['--format']
+    if opts['--zone'] == 'local':
+        zone = 'local'
+    elif opts['--zone']:
+        zone = opts['--zone']
+    else:
+        zone = None
+    prs = nldt.Parser()
+    when = nldt.moment(opts['--when'])
+    if expr in ['today']:
+        ref = prs(expr, start=when)
+        print(ref(fmt, tz=zone))
---
+++ tests/test_cmdl.py
@@ -0,0 +1,56 @@
+import nldt
+import pytest
+import tbx
+import time
+
+
+# -----------------------------------------------------------------------------
+def ftime(fmt, local=False, anchor=None):
+    """
+    Provide an oracle for test_cmdline
+    """
+    point = nldt.moment(anchor)
+    if local:
+        tm = time.localtime(point.epoch())
+    else:
+        tm = time.gmtime(point.epoch())
+    rval = time.strftime(fmt, tm)
+    return rval
+
+
+# -----------------------------------------------------------------------------
+@pytest.mark.parametrize("cmd, exp", [
+    ('nldt today', ftime('%Y-%m-%d')),
+
+    ('nldt -z local today', ftime('%Y-%m-%d', local=True)),
+
+    ('nldt -w "2000.1231 09:07:43" today', '2000-12-31'),
+
+    ('nldt -z local -f "%Y.%m%d %H:%M" today', ftime('%Y.%m%d %H:%M', True)),
+
+    ('nldt -f "%Y.%m%d %H:%M" today', ftime('%Y.%m%d %H:%M')),
+
+    ('nldt -z local -w "2000.1231 09:07:43" today', '2000-12-31'),
+
+    ('nldt -f "%Y.%m%d %H:%M:%S" -w 978253663 today',
+     '2000.1231 09:07:43'),
+
+    ('nldt -z local -f "%Y.%m%d %H:%M:%S" -w 978275263 today',
+     '2000.1231 10:07:43'),
+    ])
+def test_cmdline(cmd, exp):
+    """
+    Test nldt on the command line
+    LOC ANC FMT
+     0   0   0     nldt    today
+     0   0   1     nldt -f '%c' today
+     0   1   0     nldt -w '2005.0302' today
+     0   1   1     nldt -w '2005.0302' -f '%c' today
+     1   0   0     nldt -L today
+     1   0   1     nldt -L -f '%c' today
+     1   1   0     nldt -L -w '2006.0420' today
+     1   1   1     nldt -L -w '2006.0420' -f '%c' today
+    """
+    pytest.debug_func()
+    result = tbx.run(cmd)
+    assert result.strip() == exp
