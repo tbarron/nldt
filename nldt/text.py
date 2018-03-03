2dba901fee03b3f09184ffbbda5f6d8f7dc3beaf
Tom Barron <tusculum@gmail.com>
2018-03-03 15:36:54 -0500

    "Unsupported" is all lower case in the version python produces

--- nldt/text.py
+++ nldt/text.py
@@ -2,6 +2,6 @@ txt = {}
 txt['mctor_001'] = "If start or end is specified, both must be"
 txt['invtup'] = "Invalid tm tuple"
 txt['optypes_01'] = "Unsupported operand types(s): try 'moment' - 'duration'"
-txt['optypes_02'] = "Unsupported operand types(s): '{}' and '{}'"
+txt['optypes_02'] = "unsupported operand types(s): '{}' and '{}'"
 txt['start_inv01'] = "start only valid in ceiling/floor when unit='week'"
 txt['start_inv02'] = "start must be a weekday name or abbreviation"
