9ee6f83d51c0e9235cfa9db1d6cef265c3d3b7ba
Tom Barron <tusculum@gmail.com>
2018-03-15 09:46:39 -0400

    Move tz_context keyerr test into the bugs file

--- tests/test_06_bugs.py
+++ tests/test_06_bugs.py
@@ -1,5 +1,8 @@
+from nldt import moment as M
 import nldt
+import os
 import pytest
+from nldt.text import txt
 
 
 # -----------------------------------------------------------------------------
@@ -17,3 +20,22 @@ def test_bug_pctsec():
     assert now.epoch() == int(now("%s"))
 
 
+# -----------------------------------------------------------------------------
+def test_tz_context_keyerr():
+    """
+    Test for 'with nldt.tz_context(ZONE)' when 'TZ' is not in os.environ (so
+    pre-fix, 'del os.environ['TZ']' throws a KeyError)
+    """
+    pytest.debug_func()
+    tzorig = None
+    if 'TZ' in os.environ:
+        tzorig = os.getenv('TZ')
+
+    with nldt.tz_context('US/Pacific'):
+        then = M(txt['date02'], itz='US/Pacific')
+        assert then("%F %T", otz='US/Pacific') == txt['date02']
+
+    if tzorig:
+        os.environ['TZ'] = tzorig
+    elif 'TZ' in os.environ:
+        del os.environ['TZ']
