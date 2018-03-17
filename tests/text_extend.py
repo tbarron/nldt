e7ae692caf632123cd83ce90e83680e62c034ce0
Tom Barron <tusculum@gmail.com>
2018-03-17 15:23:44 -0400

    Catalog partition for strings only used in tests

---
+++ tests/text_extend.py
@@ -0,0 +1,9 @@
+from nldt.text import txt
+
+
+txt['rgx-mon'] = "Monday is the first thing in this list"
+txt['rgx-tue'] = "The next day is Tuesday"
+txt['rgx-wed'] = "In the middle Wednesday appears"
+txt['rgx-thu'] = "On Thursday to test multiples we pick up Saturday too"
+txt['rgx-fri'] = "Friday is the week end but not the weekend"
+txt['rgx-sun'] = "Sunday and we're done"
